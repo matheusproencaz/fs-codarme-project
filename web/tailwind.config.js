8d491ccc9132715f6ef217d4302ce91f3a17537e
Matheus Proenca <matheusproencaz@gmail.com>
2022-06-10 19:29:02 -0300

    Teste Vercel 6

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,19 +1,20 @@
 const brandColors = {
   birdBlue: '#1D9BF0',
-  platinium: '#E7E9EA',
+  platinum: '#E7E9EA',
   silver: '#71767B',
   onix: '#333639',
-  richBlack: '#15202B',
+  richBlack: '#15202B'
 }
 
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      colors:{
+      colors: {
         ...brandColors,
+
         backgroundColor: brandColors.richBlack,
-        textColor: brandColors.platinium
+        textColor: brandColors.platinum
       }
     },
   },
